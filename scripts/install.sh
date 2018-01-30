c10153e88dfdb60da8395ad6e41135683c55a749
Mendez, Marcos <MARCOS_MENDEZ@homedepot.com>
2018-01-30 11:49:05 -0500

    removes old scripts

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-PROJECT_DIR="$(cd "$(dirname "$0")/.."; pwd)"
-
-go build -o ${PROJECT_DIR}/bin/osx/rolling-restart-plugin-darwin
-cf uninstall-plugin RollingRestartPlugin || true
-cf install-plugin ${PROJECT_DIR}/bin/osx/rolling-restart-plugin-darwin -f
